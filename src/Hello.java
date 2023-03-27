51448288d4974251295c0889b4db087e70b7fe6c
taegeun1111 <122959190+taegeun1111@users.noreply.github.com>
2023-03-27 10:38:11 +0900

    20230327_ver1

---
+++ src/Hello.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("안녕하세요");
+        int i = 100;
+        String s = "김철수";
+        double random = Math.random();
+        Scanner sc = new Scanner(System.in);
+
+
+    }
+}
